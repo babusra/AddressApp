42739d1669d61870a4fdde59516218e1ffa3b1a9
Büşra Arıgün <busrarigun@gmail.com>
2024-10-12 18:34:37 +0300

    AddressListScreen updated

--- src/screens/AddressListScreen/index.tsx
+++ src/screens/AddressListScreen/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
+import Header from '../../components/Header';
+import AddressItem from '../../components/AddressItem';
+import AddressList from '../../components/AddressList';
 
 const AddressListScreen: React.FC = () => {
+  const data = [
+    {
+      cityName: 'İstanbul',
+      districtName: 'Kağıthane',
+      addressDetail: 'Çelebi Cad',
+      addressTitle: 'Ev',
+      id: '1',
+    },
+    {
+      cityName: 'İstanbul',
+      districtName: 'Kağıthane',
+      addressDetail: 'Çelebi Cad',
+      addressTitle: 'Ev',
+      id: '2',
+    },
+  ];
   return (
     <View>
-      <Text>AddressListScreen</Text>
+      <Header />
+      <AddressList addressData={data}/>
     </View>
   );
 };
