6dc355302dd0873cf056680d76f994e383190c27
Büşra Arıgün <busrarigun@gmail.com>
2024-10-12 16:38:08 +0300

    AddressListScreen created

---
+++ src/screens/AddressListScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AddressListScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>AddressListScreen</Text>
+    </View>
+  );
+};
+
+export default AddressListScreen;
