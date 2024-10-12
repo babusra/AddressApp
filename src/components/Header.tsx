b8923f393c2c12ac99573f511f36bd80d2208900
Büşra Arıgün <busrarigun@gmail.com>
2024-10-12 18:33:54 +0300

    Header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {moderateScale} from '../constants/Dimensions';
+import {Colors} from '../constants/Colors';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const Header: React.FC = () => {
+  return (
+    <LinearGradient
+      colors={[Colors.gradientFirst, Colors.gradientSecond]}
+      style={styles.linearGradient}>
+      <SafeAreaView>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingVertical: moderateScale(20),
+          }}>
+          <TouchableOpacity style={{position: 'absolute', left: 0}}>
+            <Icon name="left" size={18} color={Colors.grey} />
+          </TouchableOpacity>
+          <Text
+            style={{
+              lineHeight: 20,
+              textAlign: 'center',
+              color: Colors.grey,
+              fontSize: 16,
+            }}>
+            Adreslerim
+          </Text>
+        </View>
+        <Text
+          style={{
+            textAlign: 'center',
+            color: Colors.white,
+            fontSize: 20,
+            paddingVertical: 10,
+          }}>
+          Adres Bilgilerin
+        </Text>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    height: moderateScale(180),
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
