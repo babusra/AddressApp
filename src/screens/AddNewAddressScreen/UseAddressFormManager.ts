050d0b259329b2e681581cde3adda82d471d87d2
Büşra Arıgün <busrarigun@gmail.com>
2024-10-14 02:28:38 +0300

    AddressForm custom hook created

---
+++ src/screens/AddNewAddressScreen/UseAddressFormManager.ts
@@ -0,0 +1,84 @@
+import {useReducer, useState, useEffect} from 'react';
+import {navigation} from '../../navigation/rootNavigation';
+import {postAddress} from '../../services/api/addressApi';
+import {getCities} from '../../services/api/cityApi';
+import useFetch from '../../services/hooks/useFetch';
+import {IAddressType, FormAction} from '../../types/IAddressType';
+import {isFormValid} from '../../utils/FormValidation';
+
+const UseAddressFormManager = () => {
+  const formReducer = (
+    state: IAddressType,
+    action: FormAction,
+  ): IAddressType => {
+    switch (action.type) {
+      case 'SET_ADDRESS_TITLE':
+        return {...state, addressTitle: action.payload};
+      case 'SET_CITY':
+        return {...state, cityName: action.payload};
+      case 'SET_DISTRICT':
+        return {...state, districtName: action.payload};
+      case 'SET_DETAILS':
+        return {...state, addressDetail: action.payload};
+      case 'RESET':
+        return {
+          addressTitle: '',
+          cityName: '',
+          districtName: '',
+          addressDetail: '',
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(formReducer, {
+    addressTitle: '',
+    cityName: '',
+    districtName: '',
+    addressDetail: '',
+  });
+  const {data, loading, error} = useFetch(getCities);
+
+  const [visible, setVisible] = useState(false);
+  const [visible2, setVisible2] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const [errors, setErrors] = useState({
+    addressTitle: false,
+    cityName: false,
+    districtName: false,
+    addressDetail: false,
+  });
+  const onSave = async () => {
+    await postAddress(state);
+    setVisible2(true);
+
+    setTimeout(() => {
+      setVisible2(false);
+      navigation.goBack();
+    }, 5000);
+  };
+
+  useEffect(() => {
+    const valid = isFormValid(state);
+
+    setIsButtonDisabled(!valid);
+  }, [state]);
+  return {
+    formReducer,
+    data,
+    state,
+    onSave,
+    dispatch,
+    errors,
+    visible,
+    setVisible,
+    visible2,
+    setVisible2,
+    isButtonDisabled,
+    setErrors,
+  };
+};
+
+export default UseAddressFormManager;
