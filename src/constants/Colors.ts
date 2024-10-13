fcc96abf28cc0d05c654c2a431bdbc00e8d98501
Büşra Arıgün <busrarigun@gmail.com>
2024-10-13 14:39:26 +0300

    new colors added

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -5,5 +5,7 @@ export const Colors = {
   grey:'#C2BBCF',
   primaryGrey:"#EEF0F4",
   secondaryGrey:"#E6E9EE",
-  buttonGreen:"#019693"
+  buttonGreen:"#019693",
+  underLine:"#F9F9FB",
+  buttonGrey:"#AAB5C1"
 };
